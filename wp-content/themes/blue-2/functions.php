2b29ac791ba6407424a6a49c4ca44de6dee30f5c
Aftab Patel <pate2003@mylaurier.ca>
2026-04-03 23:38:25 +0000

    commit all changes

--- wp-content/themes/blue-2/functions.php
+++ wp-content/themes/blue-2/functions.php
@@ -11,8 +11,8 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
     wp_enqueue_script('custom_javascript0', get_stylesheet_directory_uri() . '/customElements.js');
-    wp_enqueue_script_module('custom_javascript1',"https://aftxb900.github.io/UX-308-Assignment-1/fab.js");
-    wp_enqueue_script_module('custom_javascript2',"https://aftxb900.github.io/UX-308-Assignment-1/chat.js");
+    wp_enqueue_script_module('custom_javascript1',"https://aftxb900.github.io/Assignment-1-UX-308/fab.js");
+    wp_enqueue_script_module('custom_javascript2',"https://aftxb900.github.io/Assignment-1-UX-308/chat.js");
 }
 //
 // Your code goes below
